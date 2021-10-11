28a90c83caab51f6c9139c85f30f198795d8e9bf
Roman Nikolaenkov <roman@Romans-MacBook-Pro.local>
2021-10-10 18:22:13 -0700

    3 Restful APIs 3. Parsing requests path

---
+++ 3-restful-api/3-parsing-req-paths/index.js
@@ -0,0 +1,22 @@
+// Dependencies
+const http = require('http');
+const url = require('url')
+
+// The server should respond to all requests with a string
+const server = http.createServer((req, res) => {
+    // Get the url and parse it
+    const parsedUrl = url.parse(req.url, true)
+    // Get path
+    const path = parsedUrl.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+    // Send the response
+    res.end('Hello World!\n');
+
+    // Log the request/response
+    console.log('Request received on path: '+trimmedPath);
+});
+
+
+
+// Start the server and have it to listen on PORT 3000
+server.listen(3000, () => console.log(`Listening on PORT 3000`))
