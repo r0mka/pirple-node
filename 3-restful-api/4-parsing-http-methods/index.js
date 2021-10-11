b64145e2ba6c826ef5ed36ec60245c41ace7d559
Roman Nikolaenkov <roman@Romans-MacBook-Pro.local>
2021-10-10 19:59:05 -0700

    Parsing req paths, http methods, query strings, headers

---
+++ 3-restful-api/4-parsing-http-methods/index.js
@@ -0,0 +1,24 @@
+// Dependencies
+const http = require("http");
+const url = require("url");
+
+// The server should respond to all requests with a string
+const server = http.createServer((req, res) => {
+  // Get the url and parse it
+  const parsedUrl = url.parse(req.url, true);
+  // Get path
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, "");
+  const method = req.method.toLowerCase();
+
+  // Send the response
+  res.end("Hello World!\n");
+
+  // Log the request/response
+  console.log(
+    "Request received on path: " + trimmedPath + " with method: " + method
+  );
+});
+
+// Start the server and have it to listen on PORT 3000
+server.listen(3000, () => console.log(`Listening on PORT 3000`));
